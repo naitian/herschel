9a343889ad5c8168f27125238be6d1dbbff4d673
Naitian Zhou <naitian@naitian.org>
2019-05-20 10:55:00 -0400

    Add User Facing Pages

---
+++ herschel/apps/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Magazine(models.Model):
+
+    """Docstring for Magazine. """
+    title = models.CharField(max_length=50)
+    issue = models.IntegerField()
+    url = models.URLField()
+    img_url = models.URLField()
+
+    def __str__(self):
+        return "{} (Issue {})".format(self.title, self.issue)
---
+++ herschel/apps/main/views.py
@@ -0,0 +1,17 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
+from .models import Magazine
+
+# Create your views here.
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['magazines'] = Magazine.objects.all()
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "main/about.html"
